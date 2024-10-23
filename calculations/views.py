985ea41b0166c964365f33f7530248b92b1a0a9f
rmukaila <55180284+rmukaila@users.noreply.github.com>
2024-10-23 23:04:23 +0300

    added initial endpoint names

--- calculations/views.py
+++ calculations/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def calculations(request):
     return HttpResponse ("Hello, world. You're at the calculations index.")
+
+# endpoint for creating appartment including handling creating
+# bill shares, adding members to the apartment etc
+def create_apartment(request):
+    pass
+
+# endpoint for adding members to an apartment
+def add_member(request):
+    pass
+
+# endpoint for adding bill shares to an apartment
+def add_bill_share(request):
+    pass    
